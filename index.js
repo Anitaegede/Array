96e931a8ff2d60c0035b1c17e8e5aac9f5ddeefd
Anitaegede <debbyat2008@gmail.com>
2022-07-31 11:49:14 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-/*
-var a = 2
-let b = 3
-var a = 1
-let b = 0
-*/
-let array = [1,2,3]
-array[2] ="three"
-console.log(array)
-let nArray = [2,3,4,"love","Best"]
-nArray[2]="Wow,I did it"
-console.log(nArray)
-let eq = 2=="2"
-console.log(eq)
-let eql = 2==="2"
-console.log(eql)
-if(12){
-    console.log("you're eligibile for codeville")
-} 
-if(15)
- {
-    console.log("you're eligibile for codeville")
+console.log("Hello")
+let n="Anita"
+let surname="Egede"
+let two=n+" "+surname                    
+console.log(two)
+let x=2
+let y=4
+let add=x+y
+let sub=y-x
+let div=y/x
+let mul=y*x
+console.log(mul)
+console.log(add)
+console.log(sub)
+console.log(div)
+let equals= x==y
+console.log(equals)
+let notequals=x!=y
+console.log(notequals)
+let greaterthan=x>y
+console.log(greaterthan)
+let lessthan=x<y
+console.log(lessthan)
+let win
+console.log(win)
+let person =null;
+let emptyArray = []
+let numbers = [1,2,3]
+let randomArray = [1,"Best",true,null] 
+let myName = "Anita"
+console.log(myName)
+console.log(numbers)
+console.log(numbers[2])
+console.log(randomArray[3]) 
+console.log(numbers.length)
+console.log(randomArray.length)
+console.log(emptyArray.length)
+let newArray =[5,6,1,2,5,6,7,8,10,100,0,13]
+let lastitemposition=newArray.length-1
+console.log(newArray[lastitemposition])
+let newObject={}
+let personObject={
+    name:"Anita Egede",
+    age :13,
+    job :"Junior Software Developer",
+    isrich:true
 }
+console.log(personObject) 
+console.log(personObject.name)                                    
+console.log(personObject.age)
+console.log(personObject.job)
+console.log(personObject["name"])
+console.log(personObject["age"])
+console.log(personObject["job"])
---
+++ solution.js
@@ -0,0 +1,6 @@
+let cat = {
+    haircolor:"brown",
+    eyecolor:"black"
+}
+cat.gender="female"
+console.log(cat)
